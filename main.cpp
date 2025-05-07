93400f47e42e134d33d2d07794b6bdd5770ab598
DESKTOP-5FBE16T\User <sokaman@naver.com>
2025-05-07 11:01:49 +0900

    4th commit

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
 #include "gmock/gmock.h"
 #include "similaritycheck.cpp"
 
-TEST(SimilarityCheck, Similar) {
-	Account account;
-	int actual = account.check();
-	EXPECT_EQ(100, actual);
+using namespace testing;
 
-	EXPECT_EQ(1000, 1000);
+class LengthFixture : public Test {
+public:
+    LengthChecker app;
 
+    void lengthCheck(int expected, string input1, string input2) {
+        int ret = app.getLengthPoint(input1, input2);
+        EXPECT_EQ(expected, ret);
+    }
+};
+
+TEST_F(LengthFixture, MaxLengthPoint) {
+    lengthCheck(60, "ABC", "TER");
+    lengthCheck(60, "SDFEW", "DFEWQ");
+    lengthCheck(60, "WERWQQW", "GCFAERQ");
 }
 
-
-TEST(SimilarityCheck, Similar2) {
-	EXPECT_EQ(1000, 1000);
-	EXPECT_EQ(1000, 1000);
-	EXPECT_EQ(1000, 1000);
-	EXPECT_EQ(1000, 1000);
-	EXPECT_EQ(1000, 1000);
+TEST_F(LengthFixture, MinLengthPoint) {
+    lengthCheck(0, "ABCRITUEIR", "TER");
+    lengthCheck(0, "AR", "TEWERR");
+    lengthCheck(0, "A", "TE");
+    lengthCheck(0, "AR", "T");
 }
 
+TEST_F(LengthFixture, BiggerThenAToB) {
+    lengthCheck(20, "ABCRE", "TER");
+    lengthCheck(48, "TEQYVR", "ABCRE");
+}
+
+TEST_F(LengthFixture, BiggerThenBToA) {
+    lengthCheck(48, "ABCRE", "TEQYVR");
+    lengthCheck(20, "TER", "ABCRE");
+}
 
 int main() {
-	::testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
+    testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
 }
-
-
